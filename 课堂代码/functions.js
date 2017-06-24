d3b3ae11b17a46cb772ea50f0f7b353da8806609
tombsliu <tombsliu@163.com>
2017-06-24 09:55:38 +0800

    第五天课堂代码 第一次复习 加创建节点练习

---
+++ 课堂代码/functions.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tombsliu on 2017/6/24.
+ */
+
+
+function getRandomColor() {
+        //(1) red / blue / green  rbg(r,g,b) 0-255
+        //(2) 随机数 Math.random() --> [0,1) * num
+        //(3) 返回组合后的结果
+        var r = parseInt(Math.random() * 256);
+        var g = parseInt(Math.random() * 256);
+        var b = parseInt(Math.random() * 256);
+
+        return "rgb(" + r +","+ g +","+ b +")";
+        //Math.random() -->[0,1) *255
+
+}
